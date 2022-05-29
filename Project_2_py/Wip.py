c2b8571b1ceb6bb1c7219ae748907564d7fe4d90
MLiuk <92759081+marcolucchini@users.noreply.github.com>
2022-05-29 17:27:17 +0200

    Streamlit v1.1

---
+++ Project_2_py/Wip.py
@@ -0,0 +1,114 @@
+# %%
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Bagging
+from sklearn.model_selection import train_test_split
+
+# Bagged Trees Regressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier, BaggingRegressor
+from sklearn.preprocessing import MinMaxScaler
+
+#%%
+colNames = ["Age","Gender", "Family", "Fin.Edu.", "Risk", "Income", "Wealth"]
+colMeans = [33, "Male", 2, 0.41, 0.35, 53, 66]
+colMin = [18, "Female", 1, 0, 0, 1, 1]
+colMax = [100, "Male", 5, 1, 1, 400, 2200]
+
+# %%
+######### versione 1 Bg ######
+df = pd.read_excel('Needs.xls')
+df.head()
+X = df.iloc[:,1:8]
+y = df.iloc[:,9]
+# %%
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.3)
+reg = BaggingRegressor(n_estimators=100, random_state = 0)
+reg.fit(X_train, y_train)
+y_test_pred = reg.predict(X_test)
+score = reg.score(X_test, y_test)
+print(score)
+estimator_range = [1] + list(range(10, 150, 20))
+scores = []
+sample_range = list([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+
+for sample in list(range(10)):
+    reg = BaggingClassifier(DecisionTreeClassifier(), n_estimators=60,  max_samples= (sample+1)/10, max_features = 1.0)
+    reg.fit(X_train, y_train)
+    scores.append(reg.score(X_test, y_test))
+plt.figure(figsize = (10,7))
+plt.plot(estimator_range, scores)
+
+plt.xlabel('n_estimators', fontsize =20)
+plt.ylabel('Score', fontsize = 20)
+plt.tick_params(labelsize = 18)
+plt.grid()
+bg = BaggingClassifier(DecisionTreeClassifier(), max_samples= 0.5, max_features = 1.0, n_estimators = 20)
+bg.fit(X_train,y_train)
+bg.score(X_test,y_test)
+bg.predict(X_test.loc[1,:])
+###########################
+
+#%%
+class Client():
+
+    def __init__(self):
+        self.client = [0]*7
+        self.income = 0
+        self.acc = 0
+
+# %%
+man = Client()
+
+# %%
+#### Baggede model ######
+df = pd.read_excel('Needs.xls')
+scaler = MinMaxScaler()
+scaler.fit(df)
+df = pd.DataFrame(scaler.transform(df))
+X = df.iloc[:,1:8]
+y_inc = df.iloc[:,8] # Income
+y_acc = df.iloc[:,9] # Accumulation
+X_train, X_test, y_train_inc, y_test_inc = train_test_split(X, y_inc, random_state=0, test_size=0.3)
+X_train, X_test, y_train_acc, y_test_acc = train_test_split(X, y_acc, random_state=0, test_size=0.3)
+
+bg_inc = BaggingClassifier(DecisionTreeClassifier(), max_samples= 0.5, max_features = 1.0, n_estimators = 20)
+bg_inc.fit(X_train,y_train_inc)
+print(bg_inc.score(X_test,y_test_inc))      # 0.792
+
+bg_acc = BaggingClassifier(DecisionTreeClassifier(), max_samples= 0.5, max_features = 1.0, n_estimators = 20)
+bg_acc.fit(X_train,y_train_acc)
+print(bg_acc.score(X_test,y_test_acc))      # 0.800
+#bg_inc.predict(X_test)
+############ bg #############
+
+# %%
+import joblib
+# Save the model as a pickle in a file
+joblib.dump(bg_inc, 'bg_inc.pkl')
+joblib.dump(bg_acc, 'bg_acc.pkl')
+
+#%%
+bg_inc = joblib.load('bg_inc.pkl')
+bg_acc = joblib.load('bg_acc.pkl')
+
+# %%
+from sklearn.linear_model import LinearRegression
+#%%
+
+###### Linear regression #######
+id = [1,2,3,4,6,7]
+X_Risk = df.iloc[:,id]
+y_risk = df.iloc[:,5]
+model = LinearRegression().fit(X_Risk, y_risk) 
+r_sq = model.score(X_Risk, y_risk)
+print('coefficient of determination:', r_sq)  # 0.5130044646191534
+print('intercept:', model.intercept_)
+print('slope:', model.coef_) 
+y_pred = model.predict(X_Risk)
+joblib.dump(model, 'lm_risk.pkl')
+################################
--- Project_2_py/main_Streamlit.py
+++ Project_2_py/main_Streamlit.py
@@ -2,36 +2,23 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import joblib
-from sklearn.model_selection import train_test_split
-
-# Bagged Trees Regressor
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.linear_model import LinearRegression
-
 
 # Interfaccia web
 st.title('Product suggestion by client Needs')
 
-
-#  fix this description
 st.markdown("""
 This web app helps you to suggest the best product based on the client profile.
-It take in account: Age, Gender, Income, Wealth, ...
+It take in account: Age, Gender, Family members, Financial education, Income and Wealth.
 """)
 
-
-colNames = ["Age", "Gender", "Family", "Fin.Edu.", "Risk", "Income", "Wealth"]
+# Utils:
+colNames = ["Age", "Gender", "Family Members", "Financial Education", "Risk", "Income", "Wealth"]
 colMeans = [33, "Male", 2, 0.41, 0.35, 53, 66]
 colMin = [18, "Female", 1, 0.00, 0.00, 1, 1]
 colMax = [100, "Male", 5, 1.00, 1.00, 400, 2200]
 gender = ["Male", "Female"]
 
-for i in range(len(colNames)):
-    if colNames[i] not in st.session_state:
-        st.session_state[colNames[i]] = colMeans[i]
-
-
+# Client to manage informations
 class Client():
 
     def __init__(self):
@@ -39,26 +26,33 @@ class Client():
         self.income = 0
         self.accumulation = 0
         self.suggested = pd.DataFrame
-
+        self.risk = 0
 
 man = Client()
 
+# load models for the project
 bg_inc = joblib.load('bg_inc.pkl')      # score = 0.792
 bg_acc = joblib.load('bg_acc.pkl')      # score = 0.796
-lm_risk = joblib.load('lm_risk.pkl')    # r_sq = 0.512
+lm_risk = joblib.load('lm_risk.pkl')    # r_sq = 0.513
 Products = pd.read_excel('Needs.xls', sheet_name="Products")
 
+# Inizialize variable with means
+for i in range(len(colNames)):
+    if colNames[i] not in st.session_state:
+        st.session_state[colNames[i]] = colMeans[i]
 
+
+# Functions
 def CalcRisk():
-
+    # Estimate risk based on other parameters
     tmp = pd.DataFrame(man.client).T
     id = [0, 1, 2, 3, 5, 6]
     tmp = tmp.iloc[:, id]
-    y_pred = lm_risk.predict(tmp)
-
-    return y_pred
+    man.risk = lm_risk.predict(tmp)
+    return man.risk
 
 def refactor_solution():
+    # factor product df for presentation
     tmp = man.suggested
     tmp["Type"] = ["Income" if i == True else "Accumulation" for i in tmp.Income == 1]
     tmp = tmp[["IDProduct", "Type", "Risk", "Description"]]
@@ -66,6 +60,7 @@ def refactor_solution():
     man.suggested = tmp.sort_values(by=['Risk'], ascending=False)
 
 def scale():
+    # scale client before predicting
     if int(man.client[1] == "Male"):
         man.client[1] = 1
     else:
@@ -76,8 +71,8 @@ def scale():
 
 
 def pred():
+    # predict client need for inc or acc product
     st.write(f"Il soggeto scelto è: {man.client}")
-    # vanno prima riscalate le variabili
     scale()
     st.write(f"Riscalato diventa: {man.client}")
     risk_pred = CalcRisk()
@@ -90,10 +85,11 @@ def pred():
     refactor_solution()
     return
 
-
+# Sidebar
 with st.sidebar.form(key='my_form'):
     st.subheader('Client profile')
 
+    # sliders
     for i in range(len(colNames)):
         if (i == 1):
             man.client[i] = st.selectbox('Gender', gender, key=colNames[i])
@@ -106,9 +102,9 @@ with st.sidebar.form(key='my_form'):
     submit_button = st.form_submit_button(label='Calculate!', on_click=pred())
 
 
-st.write(f"Your man wants: income: {man.income}, accumulation: {man.accumulation}")
-
-
+# Show data
+st.write(f"The estimate need are: income: {man.income}, accumulation: {man.accumulation}")
+st.write(f"The estimated risk is: {man.risk}")
 st.write(f"Suggested products are:")
 st.dataframe(man.suggested)
 
@@ -116,4 +112,4 @@ st.dataframe(man.suggested)
 # if st.button('Calcola piano'):
 #     st.write("Pulsante premuto")
 
-# streamlit run "/Users/marco/Library/CloudStorage/OneDrive-PolitecnicodiMilano/Polimi/4.2 Fintech - Marazzina/Final work/Project_2/main_Streamlit.py"
+# streamlit run "/Users/marco/Library/CloudStorage/OneDrive-PolitecnicodiMilano/Polimi/4.2 Fintech - Marazzina/Final work/Project_2_py/main_Streamlit.py"
